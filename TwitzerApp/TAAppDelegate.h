1444f752c30fb5d717a72dd86a453665d06c74d9
Johanna Sinkkonen <johanna.sinkkonen@openratio.com>
2014-03-27 13:12:30 +0100

    Initial Commit

---
+++ TwitzerApp/TAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAAppDelegate.h
+//  TwitzerApp
+//
+//  Created by Johanna Sinkkonen on 27/03/14.
+//  Copyright (c) 2014 Johanna Sinkkonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
